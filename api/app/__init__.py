111afb39f9ec36b669a0432a815f6fc58c2633b8
Remy Muhire <rmuhire@exuus.com>
2016-08-03 11:33:02 +0200

    Removed database files

--- api/app/__init__.py
+++ api/app/__init__.py
@@ -1,18 +1,9 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-import os
+
 
 
 app = Flask(__name__)
 
 
 
-###### DATATEBASE INITIALIZATION ##########
 
-db = SQLAlchemy(app)
-
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///asprin.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-
-#### DATABASE CREATION #####
-db.create_all()
