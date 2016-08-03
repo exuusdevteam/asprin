684aebc3b0c3aff72fa325d92fd0918a2de2adc1
Remy Muhire <rmuhire@exuus.com>
2016-08-03 11:35:12 +0200

    added sqlalchemy to models for db creation

--- api/app/model/models.py
+++ api/app/model/models.py
@@ -1,6 +1,18 @@
-from app import db
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from marshmallow import Schema, fields, ValidationError, pre_load
 from datetime import datetime
+
+app = Flask(__name__)
+
+
+
+###### DATATEBASE INITIALIZATION ##########
+
+db = SQLAlchemy(app)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///asprin.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
 
 
