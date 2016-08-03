b411e661e40b94db8899dde6bbca1b0786d6c494
Remy Muhire <rmuhire@exuus.com>
2016-08-03 10:50:57 +0200

    added add , update, delete function

--- api/app/model/models.py
+++ api/app/model/models.py
@@ -32,6 +32,15 @@ class User(db.Model):
             self.regDate = datetime.utcnow()
         self.business_id = business_id
 
+    def add(self, resource):
+        db.session.add(resource)
+        return db.session.commit()
+    def update(self):
+        return db.session.commit()
+    def delete(self, resource):
+        db.session.delete(resource)
+        return db.session.commit()
+
 
 
 class Business(db.Model):
