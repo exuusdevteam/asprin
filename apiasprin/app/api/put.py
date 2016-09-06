b0196b346362f338ba427d7ae92c2a038d7ff93f
Remy Muhire <rmuhire@exuus.com>
2016-09-06 15:40:44 +0200

    put api update business

--- apiasprin/app/api/put.py
+++ apiasprin/app/api/put.py
@@ -9,7 +9,7 @@ from app.controller.app.getusername import *
 @app.route("/api/put/user/<int:id>/", methods=["PUT"])
 def put_user(id):
     json_data = request.get_json()
-    if not  json_data:
+    if not json_data:
         return jsonify({'message':'No input data provided'})
 
 
@@ -44,7 +44,39 @@ def put_user(id):
 
 @app.route("/api/put/business/<int:id>/", methods=["PUT"])
 def put_business(id):
-    
+    json_data = request.get_json()
+    if not  json_data:
+        return jsonify({'message':'no input data provided'})
+
+    name =  json_data['name']
+    email = json_data['email']
+    phone = json_data['phone']
+    lat = json_data['lat']
+    lon = json_data['lon']
+    address = json_data['address']
+    web = json_data['web']
+    logo = json_data['logo']
+
+    try:
+        business = Business.query.filter_by(business_id = id).first()
+
+        business.name = name
+        business.email = email
+        business.phone = phone
+        business.lat = lat
+        business.lon = lon
+        business.address = address
+        business.web = web
+        business.logo = logo
+
+        db.session.commit()
+        return jsonify({'Message':'1'})
+
+    except:
+        return jsonify({'Message':'0'})
+
+
+
 
 
 
