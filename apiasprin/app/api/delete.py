cd7ea1c0ecde805b241a261bb00fcac2d0276cef
Remy Muhire <rmuhire@exuus.com>
2016-09-06 17:11:37 +0200

    delete printer, businessand printjob

--- apiasprin/app/api/delete.py
+++ apiasprin/app/api/delete.py
@@ -15,3 +15,37 @@ def delete_user(id):
     except:
         return jsonify({'Message':'0'})
 
+@app.route("/api/delete/business/<int:id>/", methods=["DELETE"])
+def delete_business(id):
+    try:
+        Business.query.filter_by(business_id = id).delete()
+        db.session.commit()
+        return jsonify({'Message':'1'})
+    except:
+        return jsonify({'Message':'0'})
+
+
+@app.route("/api/delete/printer/<int:id>/", methods=['DELETE'])
+def delete_printer(id):
+    try:
+        Printer.query.filter_by(printer_id = id).delete()
+        db.commit()
+        return jsonify({'Message':'1'})
+    except:
+        return jsonify({'Message':'0'})
+
+@app.route("/api/delete/printjob/<int:id>", methods=['DELETE'])
+def delete_printjob(id):
+    try:
+        PrinterJob.query.filter_by(printer_job_id = id)
+        db.session.commit()
+        return jsonify({'Message':'1'})
+    except:
+        return jsonify({'Message':'0'})
+    
+
+
+
+
+
+
