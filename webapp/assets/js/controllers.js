1687f873031c3b44880b687a1f73f30b8c571a7a
Remy Muhire <rmuhire@exuus.com>
2016-09-09 11:20:28 +0200

    added ng-file-upload depedencies and remove username on upload file

--- webapp/assets/js/controllers.js
+++ webapp/assets/js/controllers.js
@@ -1,4 +1,4 @@
-var asprinApp = angular.module('asprinApp', ['ngRoute']);
+var asprinApp = angular.module('asprinApp', ['ngRoute','ngFileUpload']);
 
 asprinApp.config(['$routeProvider', function($routeProvider){
 	
@@ -43,15 +43,31 @@ asprinApp.config(['$routeProvider', function($routeProvider){
 }]);
 
 
-asprinApp.controller('asprinCtrl', function($scope){
-	$scope.upload = true;
-	$scope.loader = false;
-	
-	
-	
-	$scope.showLoader = function(){
-		alert("Remy");
-		$scope.upload = false;
-		$scope.loader = true;
+
+asprinApp.controller('pdfUploadCtrl', ['$scope', 'Upload', '$timeout', function ($scope, Upload, $timeout) {
+	$scope.uploadPic = function(file) {
+	file.upload = Upload.upload({
+		url: 'https://angular-file-upload-cors-srv.appspot.com/upload',
+		data: {file: file},
+	});
+
+	file.upload.then(function (response) {
+		$timeout(function () {
+			file.result = response.data;
+		});
+	}, function (response) {
+	if (response.status > 0)
+		$scope.errorMsg = response.status + ': ' + response.data;
+	}, function (evt) {
+			// Math.min is to fix IE which reports 200% sometimes
+		file.progress = Math.min(100, parseInt(100.0 * evt.loaded / evt.total));
+	});
 	}
-});
+}]);
+
+
+
+
+
+
+
