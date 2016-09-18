bc58e589487cb9c05864e40265fcd13481968882
Remy Muhire <rmuhire@exuus.com>
2016-09-19 01:25:00 +0200

    arrow datetime

--- apiasprin/app/controller/app/printjob.py
+++ apiasprin/app/controller/app/printjob.py
@@ -1,4 +1,5 @@
 from ...model.models import User
+import arrow
 
 
 def printJobBusiness(json_jobs):
@@ -6,5 +7,7 @@ def printJobBusiness(json_jobs):
     for item in json_jobs:
         user = User.query.get(item['user_id'])
         item['names'] = user.names
+        date = arrow.get(item['regDate'])
+        item['regDate'] = date.to('Africa/Kigali').humanize()
         data['PrintJob'].append(item)
     return data
