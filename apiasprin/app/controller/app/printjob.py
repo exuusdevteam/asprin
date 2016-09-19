f3b435231c39731c4981bf8e8c70bb8b2b91c996
Remy Muhire <rmuhire@exuus.com>
2016-09-19 04:10:07 +0200

    storage calculation

--- apiasprin/app/controller/app/printjob.py
+++ apiasprin/app/controller/app/printjob.py
@@ -4,12 +4,19 @@ from ..pdfalgo.size import humansize
 
 
 def printJobBusiness(json_jobs):
-    data = {'PrintJob':[]}
+    data = {'storage':0,'percentage':0,'PrintJob':[]}
+    storage = 0
     for item in json_jobs:
         user = User.query.get(item['user_id'])
+        storage += int(item['size'])
         item['names'] = user.names
         item['size'] = humansize(item['size'])
         date = arrow.get(item['regDate'])
         item['regDate'] = date.to('Africa/Kigali').humanize()
         data['PrintJob'].append(item)
+
+
+
+    data['storage']= humansize(round(storage))
+    data['percentage'] = round(((float(data['storage'].split(" ")[0]) * 100)/5000),2)
     return data
