eeb61de23e4bcc5074beb798fd2c26b79f2bc38d
Remy Muhire <rmuhire@exuus.com>
2016-09-19 02:26:11 +0200

    bytes convertions library

--- apiasprin/app/controller/pdfalgo/size.py
+++ apiasprin/app/controller/pdfalgo/size.py
@@ -1,4 +1,9 @@
-def convertSize(size):
-    megabyte = float(0.000976562)
-    convert_mb = megabyte * size
-    return convert_mb
+suffixes = ['B', 'KB', 'MB', 'GB', 'TB', 'PB']
+def humansize(nbytes):
+    if nbytes == 0: return '0 B'
+    i = 0
+    while nbytes >= 1024 and i < len(suffixes)-1:
+        nbytes = float(nbytes) / 1024
+        i += 1
+    f = ('%.2f' % nbytes).rstrip('0').rstrip('.')
+    return '%s %s' % (f, suffixes[i])
