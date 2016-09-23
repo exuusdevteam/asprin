dbafca1d369dea30252f1906305ec95c2fa9577e
Remy Muhire <rmuhire@exuus.com>
2016-09-23 10:42:54 +0200

    format user dob

--- apiasprin/app/controller/app/userdate.py
+++ apiasprin/app/controller/app/userdate.py
@@ -8,4 +8,13 @@ def usersdate(json_user):
 
 def userdate(json_user):
     json_user['regDate'] = arrow.get(json_user['regDate']).to('Africa/Kigali').humanize()
+    dob = arrow.get(json_user['dob'])
+    day = dob.format('DD')
+    month = dob.format('MMM')
+    year = dob.format('YYYY')
+
+    data = []
+    data.append({'day':day,'month':month,'year':year})
+    json_user['dob'] = data
+
     return json_user
