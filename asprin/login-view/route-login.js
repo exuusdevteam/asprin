8350c223cd54efb722d4e58c691e01363b790c8c
Remy Muhire <rmuhire@exuus.com>
2016-10-04 16:49:02 +0200

    route-login.js

--- asprin/login-view/route-login.js
+++ asprin/login-view/route-login.js
@@ -13,14 +13,50 @@ asprinDeskApp.config(['$routeProvider', function($routeProvider){
 		})
 		.when('/forget', {
 			templateUrl:'login-view/forget.html'
+		})
+		.when('/app/asprindoc',{
+			templateUrl:'app/views/asprindoc.html'
 		});
 }]);
  
 
-asprinDeskApp.controller('sgininCtrl', ['$scope','$http','$location', function($scope,$http,$location){
+asprinDeskApp.controller('sgininCtrl', ['$scope','$http','$location','$window', function($scope,$http,$location, $window){
 	$scope.loginResponse = false;
-	
+	$scope.login = function(username,password){
+		var data = '{"username": "'+username+'", "password": "'+password+'"}';
+		
+		var config = {
+			headers:{
+				'Content-Type':'application/json'
+			}
+		}
+		
+		$http.post('http://0.0.0.0:5000/api/v1/login/', data, config)
+		.success(function(data, status, header, config){
+			console.log(data);
+			if (data.auth == 0 || data.auth==2){
+				$scope.loginResponse = true;
+			}else{
+				var storeUser = storeUserAsprin(data.user.user_id, data.user.user_type);
+				if (storeUser == 1){
+					$window.location.href = '/app/index.html';
+				}
+				
+			}
+		})
+		.error(function(data, status, header, config){
+			console.log(status + " "+header);
+		});
+	}
 }]);
+
+
+
+function storeUserAsprin(User,UserType){
+	localStorage.setItem('asprin_u__', User);
+	localStorage.setItem('asprin_t__', UserType);
+	return 1;
+}
 
 
 
